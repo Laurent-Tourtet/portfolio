bf600535997371de214a17e72c0a8a5c4f919167
Laurent-Tourtet <laurent.tourtet@oclock.school>
2023-11-28 20:42:30 +0100

    refacto Logo.svelte

--- src/components/Logo.svelte
+++ src/components/Logo.svelte
@@ -1,4 +1,85 @@
 <script>
+    import { onMount } from 'svelte';
+    let items = [
+      'js',
+      'svelte',
+      'node',
+      'postgres',
+      'sass',
+      'react',
+      'github',
+      'tailwind',
+      'directus',
+      'docker'
+    ];
+  
+    onMount(() => {
+      // Add any additional logic you need on component mount
+    });
+  </script>
+  
+  <section>
+    <article class="photo">
+      {#each items as item, index}
+        <div
+          class={item}
+          style={`animation-delay: ${index * 0.5}s`}
+        ></div>
+      {/each}
+    </article>
+  </section>
+  
+  <style>
+    .photo {
+      position: absolute;
+      top: 30%;
+      right: 40%;
+      background-image: url('../assets/images/logo.png');
+      background-size: cover;
+      background-position: center;
+      width: 400px;
+      height: 400px;
+      border-radius: 50%;
+      box-shadow: 0 0 0 10px #2ecc71, 0 0 0 15px #000;
+    }
+  
+    .photo div {
+      position: absolute;
+      top: 45%;
+      left: 50%;
+      width: 100px;
+      height: 100px;
+      border-radius: 50%;
+      background-position: center;
+      background-size: cover;
+      animation: rotate 5s linear infinite;
+      transform: translate(-50%, -50%);
+    }
+  
+    .js { background-image: url('../assets/images/js.png'); }
+    .svelte { background-image: url('../assets/images/svelte.png'); }
+    .node { background-image: url('../assets/images/node.png'); }
+    .postgres { background-image: url('../assets/images/postgres.png'); }
+    .sass { background-image: url('../assets/images/sass.png'); }
+    .react { background-image: url('../assets/images/react.svg'); }
+    .github { background-image: url('../assets/images/github.png'); }
+    .tailwind { background-image: url('../assets/images/tailwind.webp'); }
+    .directus { background-image: url('../assets/images/directus.jpg'); }
+    .docker { background-image: url('../assets/images/docker.png'); }
+  
+    @keyframes rotate {
+      0% {
+        transform: translate(-50%, -50%) rotate(0deg) translate(200px) rotate(0deg);
+      }
+      100% {
+        transform: translate(-50%, -50%) rotate(360deg) translate(200px) rotate(-360deg);
+      }
+    }
+  </style>
+  
+
+
+<!-- <script>
     import { link } from 'svelte-spa-router';
     import { onMount } from 'svelte';
    
@@ -27,10 +108,10 @@
         background-image: url('../assets/images/logo.png');
         background-size: cover;
         background-position: center;
-        width: 300px;
-        height: 300px;
+        width: 400px;
+        height: 400px;
         border-radius: 50%;
-        /* border: 3px solid var( --color-link-background-active); */
+        box-shadow: 0 0 0 10px #fff, 0 0 0 15px #000;
     }
     .js {
     position: absolute;
@@ -173,4 +254,4 @@
         transform: translate(-50%, -50%) rotate(360deg) translate(200px) rotate(-360deg);
     }
 }
-    </style>
+    </style> -->
